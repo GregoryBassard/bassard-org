29eed32ba786983e49259129f987118bd0636b0f
GregoryBassard <147479289+GregoryBassard@users.noreply.github.com>
2025-04-14 12:13:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ from dash import html
 app = dash.Dash(__name__)
 app.layout = html.Div([html.H1("Hello from Dash on Render!")])
 
+server = app.server
+
 if __name__ == "__main__":
     app.run_server(debug=True)
